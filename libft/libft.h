5a3b96ab4297849cb74e0b1236143943f52edc56
L0uf0k0 <maelapougewerner@gmail.com>
2024-10-08 11:30:25 +0200

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -21,7 +21,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t num_elements, size_t element_size);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
