c88e3bbaed78b56e463e7eb7ab0b3a6e697c02b3
Mae Lapouge--Werner <malapoug@c1r2s1.42lausanne.ch>
2024-11-10 14:32:03 +0100

    added putchar in pa/b ... but missing rr rrr ss

--- srcs/push_swap/process.c
+++ srcs/push_swap/process.c
@@ -6,13 +6,13 @@
 /*   By: malapoug <malapoug@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 23:52:03 by malapoug          #+#    #+#             */
-/*   Updated: 2024/11/10 00:07:06 by malapoug         ###   ########.fr       */
+/*   Updated: 2024/11/10 14:25:43 by malapoug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"pushswap.h"
 
-void	process(t_list **lst1, t_list **lst2)
+char	*process(t_list **lst1, t_list **lst2)
 {
-
+	ft_push(lst1, lst2);
 }
--- srcs/push_swap/push_swap_utils.c
+++ srcs/push_swap/push_swap_utils.c
@@ -6,19 +6,23 @@
 /*   By: malapoug <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:48:18 by malapoug          #+#    #+#             */
-/*   Updated: 2024/11/09 23:40:56 by malapoug         ###   ########.fr       */
+/*   Updated: 2024/11/10 14:31:17 by malapoug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"push_swap.h"
 
-void	ft_swap(t_list **list)
+void	ft_swap(t_list **list, char l)
 {
 	int	temp;
 
 	temp = *((*list)->data);
 	*((*list)->data) = *((*list)->next->data);
 	*((*list)->next->data) = temp;
+	if(l == 'a')
+		ft_putchar("sa\n");
+	else
+		ft_putchar("sb\n");
 }
 
 void	ft_rotate(t_list **list)
@@ -31,6 +35,11 @@ void	ft_rotate(t_list **list)
 		ft_swap(&temp);
 		temp = temp->next;
 	}
+	if(l == 'a')
+		ft_putchar("ra\n");
+	else
+		ft_putchar("rb\n");
+
 }
 
 void	ft_rev_rotate(t_list **list)
@@ -44,8 +53,13 @@ void	ft_rev_rotate(t_list **list)
 		ft_swap(&temp);
 		temp = temp->prev;
 	}
+	if(l == 'a')
+		ft_putchar("rra\n");
+	else
+		ft_putchar("rrb\n");
+
 }
-#include <unistd.h>
+
 void	ft_push(t_list **lst1, t_list **lst2)
 {
 	if (!*lst2)
@@ -61,5 +75,10 @@ void	ft_push(t_list **lst1, t_list **lst2)
 	(*lst1)->prev = NULL;
 	(*lst2)->prev->next = (*lst2);
 	*lst2 = (*lst2)->prev;
-
+	if(l == 'a')
+		ft_putchar("pa\n");
+	else
+		ft_putchar("pb\n");
 }
+
+faire les rrr rr et ss enfin les doubles quoi
