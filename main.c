c55e83cbe38b485b4b6fca6fd5d63efc9ca1d853
l <maelapouge@gmail.com>
2024-12-09 16:35:19 +0100

    bien avance, revevnir dessus pour tout revoir et tout corriger

--- main.c
+++ main.c
@@ -37,28 +37,65 @@ char	*get_path(char **envp, char *cmd)
 	return (cmd_path);
 }
 
-int main(int ac, char **av, char **envp)
+int	pipe_exec(char *cmd_path, char *cmd, char **envp)
+{
+	char	**args;
+	int	pipefd[2];
+
+	args = ft_split(cmd);
+	if (pipe(pipefd) == -1)
+		return (ft_putstr_fd("Error in pipe", 2), -1);
+	if (exceve(cmd_path, args, envp) == -1)
+		return (ft_putstr_fd("Error in exec", 2), -1);//faire une fonction variadique pour free par exemple ici args?
+}
+
+void	redircet(int pipefd[2])
+{
+	close(pipeft[0]);
+	dup2(pipefd[1], STDOUT_ FILEN0);
+	close(pipefd[1]);
+}
+
+int	forking_cmd(int	ac, char **av, char **envp)
 {
 	char	*cmd_path;
-	int		i;
-	int		pid;
+	int	pid;
+	int	i;
+	int	pipefd[2];
+
+	if (pipe(pipefd) == -1)
+		return (ft_putstr_fd("Error in pipe", 2), -1);
+	i = 1;
+	while (i < ac - 1)
+	{
+		pid = fork();
+		if (pid < 0)
+			return (-1);
+		else if (pid == 0)
+		{
+			redirect(pipefd);
+			cmd_path = get_path(envp, av[i]); //check
+			if(pipe_exec(cmd_path, av[i], envp) == -1)
+			{
+				free(cmd_path);
+				return (ft_putstr_fd("Error in pipe_exec", 2), -1);
+			}
+		}
+		i++;
+	}//close pipefd 0 et 1 ??
+	return (0);
+}
+
+int main(int ac, char **av, char **envp)
+{
 
 	if (ac < 5 || access(av[1], R_OK) != 0 || access(av[ac - 1], W_OK) != 0)
 	{
 		ft_putstr_fd("Error main case 1", 1);
 		return (1);
 	}
-	i = 1;
-	while (i < ac - 1)
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			cmd_path = get_path(envp, av[i]); //check
-			if(exceve(cmd_path) == -1)
-				??
+	if (forking_cmd(ac, av, envp) == -1)
+		return (ft_putstr_fd("Error in forking", 2), 1);
 
-		}
-	}
 }
 
